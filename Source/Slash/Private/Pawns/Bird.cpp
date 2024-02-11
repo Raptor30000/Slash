8acb70f1a0c680f36dbcd7eb7444241678207561
raptor30000 <raptor30000@wp.pl>
2024-02-11 23:21:27 +0100

    Added SkeletonMesh to Bird and spawn it with animation on Minimal map

--- Source/Slash/Private/Pawns/Bird.cpp
+++ Source/Slash/Private/Pawns/Bird.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "Pawns/Bird.h"
 
 
@@ -12,6 +13,9 @@ ABird::ABird()
 	Capsule->SetCapsuleHalfHeight(20.f);
 	Capsule->SetCapsuleRadius(15.f);
 	SetRootComponent(Capsule);
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+	BirdMesh->SetupAttachment(GetRootComponent());
 }
 
 void ABird::BeginPlay()
