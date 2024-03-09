32ed14ebc88e5c2ab82214e784e8bf998e2e861c
raptor30000 <raptor30000@wp.pl>
2024-03-09 14:17:40 +0100

    Added collision sphere to the Item Pawn and bond it in C++

--- Source/Slash/Private/Item/Item.cpp
+++ Source/Slash/Private/Item/Item.cpp
@@ -3,6 +3,7 @@
 
 #include "Item/Item.h"
 #include "Slash\DebugMacros.h"
+#include "Components/SphereComponent.h"
 
 
 AItem::AItem()
@@ -11,6 +12,9 @@ AItem::AItem()
 
 	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
 	RootComponent = ItemMesh;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(FName("Sphere"));
+	Sphere->SetupAttachment(GetRootComponent());
 }
 
 void AItem::BeginPlay()
@@ -22,6 +26,8 @@ void AItem::BeginPlay()
 
 	float AveFloat = Avg<float>(3.45f, 7.14f);
 	UE_LOG(LogTemp, Warning, TEXT("Average of 3.45 and 7.14: %f"), AveFloat);
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSphereOverlap);
 }
 
 float AItem::TransformedSin()
@@ -32,6 +38,15 @@ float AItem::TransformedSin()
 float AItem::transformedCos()
 {
 	return Amplitude * FMath::Cos(RunningTime * TimeConstant);;;
+}
+
+void AItem::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const FString OtherActorName = OtherActor->GetName();
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 30, FColor::Red, OtherActorName);
+	}
 }
 
 void AItem::Tick(float DeltaTime)
