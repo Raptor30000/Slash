5a6308a3e271920e42f0e1659cc0939fb4e1195f
raptor30000 <raptor30000@wp.pl>
2024-04-21 21:00:51 +0200

    Changed GetHit function in HitInterface to be BlueprintNativeevent and implemented consecutive chcenges to Weapon.h anc cpp. In BP_Breakable added only calling GetHit and connected it to parent GEtHit to implement playing sound next time.

--- Source/Slash/Private/Breakable/BreakableActor.cpp
+++ Source/Slash/Private/Breakable/BreakableActor.cpp
@@ -29,7 +29,7 @@ void ABreakableActor::Tick(float DeltaTime)
 
 }
 
-void ABreakableActor::GetHit(const FVector& ImpactPoint)
+void ABreakableActor::GetHit_Implementation(const FVector& ImpactPoint)
 {
 
 }
